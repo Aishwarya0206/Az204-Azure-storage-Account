06449339916d0748b0d65acededacea313b54f62
Aishwarya B <35876177+Aishwarya0206@users.noreply.github.com>
2023-11-28 05:50:27 +0530

    Set & retrieve metadata properties

---
+++ Lab - .Net Set and Retrieve metadata properties/BlobApp/BlobApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=azstorageacc204;AccountKey=JyP4QhuFK46C3gFx2Gu4tzeck/Gd7qG+FEuc88iU3RAb8kcIseaVC2Oy4SfOchAK9mbdvW0IMif++AStNWRIKw==;EndpointSuffix=core.windows.net";
+string containerName = "sample";
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+await ReadContainerPropertiesAsync(containerClient);
+await AddContainerMetadataAsync(containerClient);
+await ReadContainerMetadataAsync(containerClient);
+
+async Task ReadContainerPropertiesAsync(BlobContainerClient container)
+    {
+
+    try
+    {
+        // Fetch some container properties and write out their values.
+        var properties = await container.GetPropertiesAsync();
+        Console.WriteLine($"Properties for container {container.Uri}");
+        Console.WriteLine($"Public access level: {properties.Value.PublicAccess}");
+        Console.WriteLine($"Last modified time in UTC: {properties.Value.LastModified}");
+    }
+    catch (Azure.RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+    }
+
+
+async Task AddContainerMetadataAsync(BlobContainerClient container)
+    {
+
+    try
+    {
+        IDictionary<string, string> metadata =
+           new Dictionary<string, string>();
+
+        // Add some metadata to the container.
+        metadata.Add("docType", "textDocuments");
+        metadata.Add("category", "guidance");
+
+        // Set the container's metadata.
+        await container.SetMetadataAsync(metadata);
+    }
+    catch (Azure.RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+    }
+
+
+async Task ReadContainerMetadataAsync(BlobContainerClient container)
+    {
+
+    try
+    {
+        var properties = await container.GetPropertiesAsync();
+
+        // Enumerate the container's metadata.
+        Console.WriteLine("Container metadata:");
+        foreach (var metadataItem in properties.Value.Metadata)
+        {
+            Console.WriteLine($"\tKey: {metadataItem.Key}");
+            Console.WriteLine($"\tValue: {metadataItem.Value}");
+        }
+    }
+    catch (Azure.RequestFailedException e)
+    {
+        Console.WriteLine($"HTTP error code {e.Status}: {e.ErrorCode}");
+        Console.WriteLine(e.Message);
+        Console.ReadLine();
+    }
+    }
